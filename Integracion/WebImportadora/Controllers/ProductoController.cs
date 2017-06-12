89f2b779ee7fcc312b7a01ee41cbf0e266fa0c8d
Max Fuentes <mfnunez36@gmail.com>
2017-06-12 17:09:44 -0400

    Commit 0
    Subiendo Ejemplo vistas Controller

---
+++ Integracion/WebImportadora/Controllers/ProductoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebImportadora.Controllers
+{
+    public class ProductoController : Controller
+    {
+        // GET: Producto
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+
+        public ActionResult Editar()
+        {
+            return View();
+        }
+
+        public ActionResult Eliminar()
+        {
+            return View();
+        }
+    }
+}
---
+++ Integracion/WebImportadora/Controllers/UsuarioController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebImportadora.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+
+        public ActionResult Editar()
+        {
+            return View();
+        }
+
+        public ActionResult Eliminar()
+        {
+            return View();
+        }
+    }
+}
